5822fbd3235510f8c1ee0f13223e2615c52c305b
George Bryant <george.s.bryant@gmail.com>
2016-11-09 17:34:33 +0000

    Added form functionality to the homepage which outputs the values to the console

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,15 @@
 let show = (req, res) => {
-    res.render('index.njk');
+  res.render('index.njk');
+};
+
+let sendForm = (req, res) => {
+  let userName = req.body.username;
+  let userEmail = req.body.useremail;
+  console.log("Username: " + userName + " Email: " + userEmail);
+  res.render('index.njk');
 };
 
 module.exports = {
-    show: show
+  show: show,
+  sendForm: sendForm
 };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let express = require("express");
 let app = express();
 let path = __dirname + '/views/';
 let nunjucks = require('nunjucks');
+let bodyParser = require('body-parser');
 
 let index = require('./controllers/index.js');
 let err404 = require('./controllers/err404.js');
@@ -24,8 +25,14 @@ let logRequest = (req, res, next) => {
 	next();
 };
 
+//This allows us to use POST (crashes without) I think by converting the body into JSON so that index.sendForm can use it
+app.use(bodyParser.json());
+//This allows us to actually see the values, as without it we get 'undefined', but I dont know how or why it works
+app.use(bodyParser.urlencoded({extended: true}));
+
 //Returns the requested page of the site to the user
 app.get("/", logRequest, index.show);
+app.post("/", logRequest, index.sendForm);
 
 app.get('/test/:name', logRequest, test.retrieve);
 app.get('/test', logRequest, test.show);
