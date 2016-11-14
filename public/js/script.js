a171960a927fea9203329a3aec7b6d712e75f6b2
George Bryant <george.s.bryant@gmail.com>
2016-11-14 15:09:01 +0000

    Added form to test page with buttons to add and remove table rows

---
+++ public/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $("#addRowBtn").click(function(e){
+      e.preventDefault();
+        $("table#testMaker").append([
+        "<tr>",
+          "<td>",
+            "<select class='form-control' name='idOrClass'>",
+              "<option>Class</option>",
+              "<option>ID</option>",
+            "</select>",
+          "</td>",
+          "<td><input type='text' class='form-control' name='elementID' placeholder='ID'/></td>",
+          "<td>",
+            "<select class='form-control' name='idOrClass'>",
+              "<option>Click</option>",
+              "<option>Text Entry</option>",
+            "</select></td>",
+          "<td><input type='text' class='form-control' name='textEntry' placeholder='Enter text here'/></td>",
+        "</tr>",
+      ].join(''));
+    });
+});
+
+$(document).ready(function(){
+  $("#dltRowBtn").click(function(e){
+    e.preventDefault();
+    if ($("table#testMaker tr").length > 1) {
+      $("table#testMaker tr:last").remove();
+    }
+  });
+});
