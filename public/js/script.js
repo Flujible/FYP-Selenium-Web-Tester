b784b02073b36d6c79b1bb63237ac155d6a529e6
George Bryant <george.s.bryant@gmail.com>
2016-11-17 11:43:55 +0000

    Altered row reproduction to correct the form's incorrect JSON object output

--- public/js/script.js
+++ public/js/script.js
@@ -2,22 +2,22 @@ $(document).ready(function(){
     $("#addRowBtn").click(function(e){
       e.preventDefault();
         $("table#testMaker").append([
-        "<tr>",
-          "<td>",
-            "<select class='form-control' name='idOrClass'>",
-              "<option>Class</option>",
-              "<option>ID</option>",
-            "</select>",
-          "</td>",
-          "<td><input type='text' class='form-control' name='elementID' placeholder='ID'/></td>",
-          "<td>",
-            "<select class='form-control' name='idOrClass'>",
-              "<option>Click</option>",
-              "<option>Text Entry</option>",
-              "<option>Assert</option>",
-            "</select></td>",
-          "<td><input type='text' class='form-control' name='textEntry' placeholder='Enter text here'/></td>",
-        "</tr>",
+          '<tr>',
+            '<td>',
+              '<select class="form-control" name="idOrClass">',
+                '<option>Class</option>',
+                '<option>ID</option>',
+              '</select>',
+            '</td>',
+            '<td><input type="text" class="form-control" name="elementID" placeholder="ID"/></td>',
+            '<td>',
+              '<select class="form-control" name="act">',
+                '<option>Click</option>',
+                '<option>Text Entry</option>',
+                '<option>Assert</option>',
+              '</select></td>',
+            '<td><input type="text" class="form-control" name="textEnt" placeholder="Enter text here"/></td>',
+          '</tr>',
       ].join(''));
     });
 });
