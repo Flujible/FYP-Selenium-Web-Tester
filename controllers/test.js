084b071a0bb4e1f113f76fc59b86c042c59bbd46
George Bryant <george.s.bryant@gmail.com>
2016-11-23 12:31:33 +0000

    Added generated GUID to each job's JSON object

--- controllers/test.js
+++ controllers/test.js
@@ -1,3 +1,5 @@
+let guid = require("guid");
+
 let show = (req, res) => {
   //Assigns URL parameters to variables
   //URL parameters get stored in an object
@@ -19,15 +21,13 @@ let retrieve = (req, res) => {
   res.render('test.njk', {name: req.params.name});
 }
 
-let sendForm = (req, res) => {
-  let userName = req.body.username;
-  let userEmail = req.body.useremail;
-  console.log("Username: " + userName + " Email: " + userEmail);
-  res.render('formAccept.njk');
-};
-
 let createTest = (req, res) => {
+  let newGuid = guid.create()
+  while (!guid.isGuid(newGuid)) {
+    newGuid = guid.create();
+  }
   let test = {
+    guid: newGuid.value,
     url: req.body.url,
     idType: req.body.idOrClass,
     id: req.body.elementID,
@@ -35,24 +35,11 @@ let createTest = (req, res) => {
     value: req.body.textEnt
   }
   console.log(test);
-
-  // let url = req.body.url;
-  // let idType = req.body.idOrClass;
-  // let id = req.body.elementID;
-  // let action = req.body.act;
-  // let text = req.body.textEnt;
-  //
-  // console.log("URL: " + url + "\n" +
-  //             "ID Type: " + idType + "\n" +
-  //             "ID: " + id + "\n" +
-  //             "Action: " + action + "\n" +
-  //             "Text: " + text + "\n");
   res.render('formAccept.njk');
 };
 
 module.exports = {
   show: show,
   retrieve: retrieve,
-  sendForm: sendForm,
   createTest: createTest
 };
