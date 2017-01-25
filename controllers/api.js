77a7349c3b0c4d7a6278cf6ddff8b4573e7392ee
George Bryant <george.s.bryant@gmail.com>
2017-01-25 17:57:42 +0000

    Built an api endpoint to show the id and status of all tests or the id and full results of a single test

---
+++ controllers/api.js
@@ -0,0 +1,34 @@
+const redisClient = require('redis').createClient(process.env.REDIS_URL);
+
+module.exports = {
+  //Show all keys with their status in an array of objects
+  index: (req, res) => {
+    redisClient.keys('*', function (err, keys) {
+      if (err) {
+        return res.error(err);
+      } else {
+        let promises = [];
+        keys.forEach(key => {
+          console.log(key);
+          promises.push(new Promise(resolve => {
+            redisClient.hget(key, 'status', (err, status) => {
+              resolve(err || { key, status });
+            });
+          }));
+        });
+        Promise.all(promises).then(results => {
+          res.send(results);
+        });
+      }
+    });
+  },
+  show: (req, res) => {
+    guid = req.params.guid;
+    redisClient.hget(guid, 'result', function (err, result) {
+      if (err) {return res.error(err);
+      } else {
+        res.send({guid, result});
+      }
+    });
+  }
+}
