e7f1d4969a0ca46ab6b32febe006f24a079046dd
George Bryant <george.s.bryant@gmail.com>
2017-02-08 17:07:40 +0000

    Added a jquery function to refresh the results table every second

--- public/js/getAllResults.js
+++ public/js/getAllResults.js
@@ -1,21 +1,36 @@
-$(document).ready(function(){
-  $.ajax({
-    type: "GET",
-    url: "api/results",
-    dataType: "json",
-    success: function (data) {
-      data.forEach(test => {
-        $("table#indexResults").append([
-          '<tr>',
-            '<td>',
-            `<a href = /results/${test.key}>`,test.key,
-            '</td>',
-            '<td>',
-              test.status,
-            '</td>',
-          '</tr>',
-      ].join(''));
-    });
+let updateTable = () => {
+  $(document).ready(function() {
+    if ($("table#indexResults").length) {
+      $.ajax({
+        type: "GET",
+        url: "/api/results",
+        dataType: "json",
+        success: function (data) {
+          let str = "";
+          data.forEach(test => {
+            let status;
+            if (test.status === 'pending') {
+              status = '<div class=loader>Loading...</div>';
+            } else {
+              status = test.status;
+            }
+            str += ([
+              '</tr>',
+              '<td class="key">',
+              `<a href = /results/${test.key}>`,test.key,'</a>',
+              '</td>',
+              '<td class="status">',
+              status,
+              '</td>',
+              '</tr>',
+            ].join(''));
+          });
+          $("table#indexResults").html('<tr><th>Test ID</th><th>Result</th><tr>' + str);
+        }
+      });
     }
   });
-});
+};
+
+updateTable();
+setInterval(updateTable, 1000);
